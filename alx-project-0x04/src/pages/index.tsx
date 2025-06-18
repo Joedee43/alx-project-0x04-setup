f541791074d7343833559010356a19e1c84eef81
joe dee <joedee4311@gmail.com>
2025-06-18 23:32:46 +0300

    update

---
+++ alx-project-0x04/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Geist, Geist_Mono } from "next/font/google";
+import Counter from "../pages/counter-app";
+
+
+export default function Home() {
+  return (
+    <div>
+     <Counter/>
+    </div>
+  );
+}
